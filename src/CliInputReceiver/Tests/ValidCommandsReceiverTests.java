847414a3cf3012c839ead6f9fa61555d558e1e59
fabbernat <fabbernat@gmail.com>
2025-10-08 17:30:02 +0200

    i21.0.4: improve Kingdom Rush CLI welcome tájékoztató message

--- src/CliInputReceiver/Tests/ValidCommandsReceiverTests.java
+++ src/CliInputReceiver/Tests/ValidCommandsReceiverTests.java
@@ -11,7 +11,7 @@ public class ValidCommandsReceiverTests {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("KR CLI started. Type 'exit' to quit.");
+    System.out.println("KR CLI started. Type 'help' to get the list of available commands. Examples: \n'add' and the tower name to add a tower; \n'kill' and the enemy name to destroy an enemy; \n'exit' to quit.");
 
     while (true) {
       System.out.print("> ");
